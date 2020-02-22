66eda38c35337a3c0e3b45de3dd3e605ae28ed83
naz k. k <f.kashandeh@gmail.com>
2020-02-22 14:25:28 -0500

    Ajouter PiocheIrrigation.java.

---
+++ src/main/java/uqam/inf5153/game/pioche/PiocheIrrigations.java
@@ -0,0 +1,20 @@
+package uqam.inf5153.game.pioche;
+
+import uqam.inf5153.game.Irrigation;
+import java.util.List;
+
+
+public class PiocheIrrigations extends Pioche<Irrigation>{
+
+    @Override
+    public Irrigation piocher(List<Irrigation> irrigations) {
+        return irrigations.get(0);
+    }
+
+
+    @Override
+    public List<Irrigation> piocher(int nbrAPiocher, List<Irrigation> irrigation){
+        throw new UnsupportedOperationException("Vous ne pouvez pas piocher plusieurs irrigation à la fois.");
+    }
+
+}
