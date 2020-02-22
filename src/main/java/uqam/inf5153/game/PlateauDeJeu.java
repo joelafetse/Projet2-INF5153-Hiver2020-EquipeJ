a953031778f9856b33c93b2eb12752706c47edab
Kokou Eyram Joel Afetse <afetse.kokou_eyram_joel@courrier.uqam.ca>
2020-02-22 10:07:28 -0500

    corriger indentation et noms de variables

--- src/main/java/uqam/inf5153/game/PlateauDeJeu.java
+++ src/main/java/uqam/inf5153/game/PlateauDeJeu.java
@@ -11,76 +11,86 @@ import java.util.List;
 
 public class PlateauDeJeu {
 
-private List<Parcelle> plateau ;
-private HashSet<Coordonnees> candidat;
+    private List<Parcelle> parcelles;
+    private HashSet<Coordonnees> positionsDisponibles;
 
 
-public PlateauDeJeu() {
-        this.plateau =new ArrayList<Parcelle>();
-        this.candidat=new HashSet<Coordonnees>();
-}
+    public PlateauDeJeu() {
+        this.parcelles =new ArrayList<Parcelle>();
+        this.positionsDisponibles =new HashSet<Coordonnees>();
+    }
 
-public int nbreElement(){
-    return plateau.size();
-}
+    public int nbreElement(){
+        return parcelles.size();
+    }
 
-public void initialiserPlateauDeJeu(){
-    Parcelle p1= new ParcelleEtang(new Coordonnees(0,0));
-    plateau.add(p1);
-    candidat.add(new Coordonnees(2,1));
-    candidat.add(new Coordonnees(2,-1));
-    candidat.add(new Coordonnees(-2,1));
-    candidat.add(new Coordonnees(-2, -1));
-    candidat.add(new Coordonnees(0,-2));
-    candidat.add(new Coordonnees(0,2));
-}
+    public void initialiserPlateauDeJeu(){
+        Parcelle p1= new ParcelleEtang(new Coordonnees(0,0));
+        parcelles.add(p1);
+        positionsDisponibles.add(new Coordonnees(2,1));
+        positionsDisponibles.add(new Coordonnees(2,-1));
+        positionsDisponibles.add(new Coordonnees(-2,1));
+        positionsDisponibles.add(new Coordonnees(-2, -1));
+        positionsDisponibles.add(new Coordonnees(0,-2));
+        positionsDisponibles.add(new Coordonnees(0,2));
+    }
 
-public void placerUneParcelle(int x, int y){
+    public void placerUneParcelle(int x, int y){
         Coordonnees c = new Coordonnees(x,y);
-        plateau.add(new ParcelleVerte(c));
-        candidat.removeIf(coord -> coord.equals(c));
-}
-
-public boolean estPositionOccupee(int x, int y){
-boolean trouve =false;
-Coordonnees c=new Coordonnees(x,y);
-int i=0;
-while(i <plateau.size() && !trouve) {
-    if (plateau.get(i)!=null && plateau.get(i).getCoordonnees()!=null && plateau.get(i).getCoordonnees().equals(c)) {
-        trouve = true;
-    }
-    i++;
-}
-    return !trouve;
-}
-
-public void mettreAjourListePosiDisp(int x, int y){
-    if (!estPositionOccupee(x + 2, y + 1)) {candidat.add(new Coordonnees(x+2,y+1));};
-    if (!estPositionOccupee(x + 2, y - 1)) {candidat.add(new Coordonnees(x+2,y-1));};
-    if (!estPositionOccupee(x - 2, y - 1)) {candidat.add(new Coordonnees(x-2,y-1));};
-    if (!estPositionOccupee(x - 2, y + 1)) {candidat.add(new Coordonnees(x-2,y+1));};
-    if (!estPositionOccupee(x, y + 2)) {candidat.add(new Coordonnees(x,y+2));};
-    if (!estPositionOccupee(x, y - 2)) {candidat.add(new Coordonnees(x,y-2));};
-}
-
-
-public void afficherLesPositionsDispo(){
-    for(Coordonnees elem: candidat)
-    {
-        System.out.println ("( "+ elem.getX() +", " +elem.getY()+" )");
+        parcelles.add(new ParcelleVerte(c));
+        positionsDisponibles.removeIf(coord -> coord.equals(c));
     }
 
-}
+    public boolean estPositionOccupee(int x, int y){
+        boolean trouve =false;
+        Coordonnees c=new Coordonnees(x,y);
+        int i=0;
+        Parcelle parcelle;
+        while(i < parcelles.size() && !trouve) {
+            parcelle = parcelles.get(i);
+            if (parcelle != null &&
+                    parcelle.getCoordonnees()!=null &&
+                    parcelle.getCoordonnees().equals(c)) {
+                trouve = true;
+            }
+            i++;
+        }
+        return !trouve;
+    }
 
-  public void afficherParcelleDeposee() {
-      int i=0;
-      while(i <plateau.size()) {
-          if (plateau.get(i)!=null && plateau.get(i).getCoordonnees()!=null) {
-              System.out.println(plateau.get(i).getCoordonnees().toString());
-         }
-          i++;
-      }
+    public void mettreAjourListePosiDisp(int x, int y){
+        if (!estPositionOccupee(x + 2, y + 1)) {
+            positionsDisponibles.add(new Coordonnees(x+2,y+1));};
+        if (!estPositionOccupee(x + 2, y - 1)) {
+            positionsDisponibles.add(new Coordonnees(x+2,y-1));};
+        if (!estPositionOccupee(x - 2, y - 1)) {
+            positionsDisponibles.add(new Coordonnees(x-2,y-1));};
+        if (!estPositionOccupee(x - 2, y + 1)) {
+            positionsDisponibles.add(new Coordonnees(x-2,y+1));};
+        if (!estPositionOccupee(x, y + 2)) {
+            positionsDisponibles.add(new Coordonnees(x,y+2));};
+        if (!estPositionOccupee(x, y - 2)) {
+            positionsDisponibles.add(new Coordonnees(x,y-2));};
+    }
 
-  }
+
+    public void afficherLesPositionsDispo(){
+        for(Coordonnees elem: positionsDisponibles)
+        {
+            System.out.println ("( "+ elem.getX() +", " +elem.getY()+" )");
+        }
+
+    }
+
+    public void afficherParcelleDeposee() {
+        int i=0;
+        while(i < parcelles.size()) {
+            if (parcelles.get(i)!=null && parcelles.get(i).getCoordonnees()!=null) {
+                System.out.println(parcelles.get(i).getCoordonnees().toString());
+            }
+            i++;
+        }
+
+    }
 
 }
