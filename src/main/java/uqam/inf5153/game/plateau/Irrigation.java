3a76e7eafb2c62256e00ad741f98232f81fdbb99
Kokou Eyram Joel Afetse <afetse.kokou_eyram_joel@courrier.uqam.ca>
2020-02-24 22:37:51 -0500

    ajouter compteur canauxIrrigations

--- src/main/java/uqam/inf5153/game/plateau/Irrigation.java
+++ src/main/java/uqam/inf5153/game/plateau/Irrigation.java
@@ -6,6 +6,7 @@ public class Irrigation {
 
     private Parcelle parcelle1;
     private Parcelle parcelle2;
+    private static int nbrCanauxIrrigation = 20;
 
     public Irrigation () {
 
@@ -22,6 +23,19 @@ public class Irrigation {
 
     public Parcelle getParcelle2 ()   {
         return parcelle2;
+    }
+
+    public static int getNbrCanauxIrrigation() {
+        return nbrCanauxIrrigation;
+    }
+
+    public static void setNbrCanauxIrrigation(int nbrCanauxIrrigationRestant) {
+        Irrigation.nbrCanauxIrrigation = nbrCanauxIrrigationRestant;
+    }
+
+    public void setParcelles(Parcelle parcelle1, Parcelle parcelle2){
+        this.parcelle1 = parcelle1;
+        this.parcelle2 = parcelle2;
     }
 
 
