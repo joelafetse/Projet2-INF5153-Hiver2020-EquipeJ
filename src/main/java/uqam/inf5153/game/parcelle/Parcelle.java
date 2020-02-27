ad703fc8efbc51443de7daf4e32bbaea96e8c257
Kokou Eyram Joel Afetse <afetse.kokou_eyram_joel@courrier.uqam.ca>
2020-02-26 19:25:37 -0500

    update parcelle

--- src/main/java/uqam/inf5153/game/parcelle/Parcelle.java
+++ src/main/java/uqam/inf5153/game/parcelle/Parcelle.java
@@ -6,13 +6,9 @@ public abstract class Parcelle {
 
     protected Coordonnees coord;
     protected boolean irriguee;
-    private voisin
 
-<<<<<<< HEAD
-    public Parcelle(Coordonnees coordonnees, boolean irriguee){
-=======
+
     public Parcelle(){
->>>>>>> 5445a52601e591bd6b181e80768406f003e60107
 
     }
 
@@ -20,23 +16,22 @@ public abstract class Parcelle {
         return this.coord;
     }
 
-<<<<<<< HEAD
     public boolean estIrriguee() {
         return irriguee;
     }
 
-=======
+
     public void setCoordonnees(Coordonnees coord) {
         this.coord = coord;
     }
 
     public abstract Couleur getCouleur();
->>>>>>> 5445a52601e591bd6b181e80768406f003e60107
+
 
     @Override
     public boolean equals(Object obj) {
         return obj instanceof Parcelle &&
-                this.coord.equals(((Parcelle) obj).coord) &&
+                this.coord.equals(((Parcelle) obj).coord) && this.getCouleur() == ((Parcelle) obj).getCouleur();
     }
 
     public boolean estAdjacent(Parcelle p){
@@ -46,39 +41,6 @@ public abstract class Parcelle {
                 (p.coord.getX() == this.coord.getX() - 2 && p.coord.getY() == this.coord.getY() - 1) ||
                 (p.coord.getX() == this.coord.getX() && p.coord.getY() == this.coord.getY() + 2) ||
                 (p.coord.getX() == this.coord.getX() && p.coord.getY() == this.coord.getY() - 2);
-    }
-
-
-    public int [][] listeDesAdjacents(Parcelle p){
-
-        int [][] listeAdjacents = new int[6][2];
-        posAdj[0][0]=p.getCoordonnees().getX()-2;
-        posAdj[0][1]=p.getCoordonnees().getY()+1;
-
-        posAdj[1][0]=p.getCoordonnees().getX();
-        posAdj[1][1]=p.getCoordonnees().getY()+2;
-
-        posAdj[2][0]=p.getCoordonnees().getX()+2;
-        posAdj[2][1]=p.getCoordonnees().getY()+1;
-
-        posAdj[3][0]=p.getCoordonnees().getX()+2;
-        posAdj[3][1]=p.getCoordonnees().getY()-1;
-
-        posAdj[4][0]=p.getCoordonnees().getX();
-        posAdj[4][1]=p.getCoordonnees().getY()-2;
-
-        posAdj[5][0]=p.getCoordonnees().getX()-2;
-        posAdj[5][1]=p.getCoordonnees().getY()-1;
-        return listeAdjacents;
-
-    }
-
-    public Parcelle[] parcellesAdjacentes(){
-        Parcelle[] parcellesAdjacentes=new Parcelle[6];
-        for (int i=0; i<6; i++){
-            if (lienParcelles[i]!=null) parcellesAdjacentes[i]= lienParcelles[i].getParcelLie(this);
-        }
-        return parcellesAdjacentes;
     }
 
 }
