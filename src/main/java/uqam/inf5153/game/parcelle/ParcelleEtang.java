672809245625ab409fbfea39efeafa8122dbddaf
Kokou Eyram Joel Afetse <afetse.kokou_eyram_joel@courrier.uqam.ca>
2020-02-26 19:24:57 -0500

    update parcelles

--- src/main/java/uqam/inf5153/game/parcelle/ParcelleEtang.java
+++ src/main/java/uqam/inf5153/game/parcelle/ParcelleEtang.java
@@ -3,8 +3,7 @@ package uqam.inf5153.game.parcelle;
 import uqam.inf5153.game.plateau.Coordonnees;
 
 public class ParcelleEtang extends Parcelle {
-    public ParcelleEtang(Coordonnees coordonnees) {
-        super(coordonnees);
+    public ParcelleEtang() {
     }
 
     /* Parcelle Étang n'a pas de couleur */
