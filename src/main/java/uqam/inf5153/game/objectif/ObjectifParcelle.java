89148ecccbfdd5957c7888392f15249b38adafc8
yazid.iharkane@gmail.com <yazid.iharkane@gmail.com>
2020-02-29 16:40:51 -0500

    Rien a ajouter

--- src/main/java/uqam/inf5153/game/objectif/ObjectifParcelle.java
+++ src/main/java/uqam/inf5153/game/objectif/ObjectifParcelle.java
@@ -3,7 +3,6 @@ package uqam.inf5153.game.objectif;
 import uqam.inf5153.game.joueur.Joueur;
 import uqam.inf5153.game.parcelle.Couleur;
 import uqam.inf5153.game.parcelle.Parcelle;
-import uqam.inf5153.game.parcelle.ParcelleVerte;
 import uqam.inf5153.game.plateau.PlateauDeJeu;
 
 public class ObjectifParcelle extends Objectif {
@@ -165,6 +164,7 @@ public class ObjectifParcelle extends Objectif {
                     }
                 }
                 break;
+
         }
         return 0;
     }
