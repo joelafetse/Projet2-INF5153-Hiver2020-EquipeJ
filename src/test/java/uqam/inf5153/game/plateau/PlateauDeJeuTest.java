29034f22fad341d746ab999afa7c28e75fe2f06c
yiharkane <yazid.iharkane@gmail.com>
2020-03-03 19:20:27 -0500

    Test de la classe PlateauDeJeu

--- src/test/java/uqam/inf5153/game/plateau/PlateauDeJeuTest.java
+++ src/test/java/uqam/inf5153/game/plateau/PlateauDeJeuTest.java
@@ -1,56 +1,55 @@
 package uqam.inf5153.game.plateau;
 
 import org.junit.Test;
+import uqam.inf5153.game.parcelle.Parcelle;
+import uqam.inf5153.game.parcelle.ParcelleEtang;
+import uqam.inf5153.game.parcelle.ParcelleVerte;
 
 import static org.junit.Assert.*;
 
 public class PlateauDeJeuTest {
 
-    @Test
-    public void nbreDeParcelles() {
-    }
 
     @Test
-    public void getParcelleEtang() {
-    }
-
-    @Test
-    public void getParcelles() {
-    }
-
-    @Test
-    public void getPositionsDisponibles() {
-    }
-
-    @Test
-    public void getReseauxIrr() {
+    public void testNbreDeParcelles() {
+    PlateauDeJeu plateau = new PlateauDeJeu();
+    assertEquals(plateau.nbreDeParcelles(),1);
     }
 
     @Test
     public void ajouterParcelle() {
+        PlateauDeJeu plateau = new PlateauDeJeu();
+        ParcelleVerte p =new ParcelleVerte();
+        plateau.ajouterParcelle(p);
+        assertEquals(plateau.nbreDeParcelles(),2);
     }
 
     @Test
-    public void getJardinier() {
+    public void testPositionOccupee() {
+        PlateauDeJeu plateau = new PlateauDeJeu();
+        Coordonnees c=new Coordonnees(0,0);
+        assertEquals(plateau.estPositionOccupee(c), true);
     }
 
     @Test
-    public void getParcelleAtPosition() {
+    public void testPositionParcelle() {
+        PlateauDeJeu plateau =new PlateauDeJeu();
+        assertEquals(plateau.positionParcelle(0,0),0);
     }
 
     @Test
-    public void estPositionOccupee() {
+    public void testMettreAjourListePosiDisp() {
+        PlateauDeJeu plateau =new PlateauDeJeu();
+        Coordonnees c = new Coordonnees(0,0);
+        plateau.mettreAjourListePosiDisp(c);
+        assertEquals(plateau.getPositionsDisponibles().size(),6);
     }
 
     @Test
-    public void positionParcelle() {
-    }
+    public void testNombreVoisinsOccupes() {
+        PlateauDeJeu plateau = new PlateauDeJeu();
+        Coordonnees c= new Coordonnees(0,0);
+        assertEquals(plateau.nombreVoisinsOccupes(c), 2);
 
-    @Test
-    public void mettreAjourListePosiDisp() {
-    }
-
-    @Test
-    public void nombreVoisinsOccupes() {
     }
 }
