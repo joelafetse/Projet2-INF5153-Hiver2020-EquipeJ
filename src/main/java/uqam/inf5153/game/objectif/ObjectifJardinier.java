2f75f18ac748124641902eef4423adf37b09d3ed
yiharkane <yazid.iharkane@gmail.com>
2020-04-13 18:29:18 -0400

    Correction de problèmes rencontés avec la classe ObjectifJardinier

--- src/main/java/uqam/inf5153/game/objectif/ObjectifJardinier.java
+++ src/main/java/uqam/inf5153/game/objectif/ObjectifJardinier.java
@@ -21,20 +21,20 @@ public class ObjectifJardinier extends Objectif {
         switch (identifiant){
             case 16:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof  AmenagementBassin && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
+                    if(p instanceof  AmenagementBassin && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
                         return 5;
                 }
                 break;
             case 17:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof Parcelle && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
+                    if(p instanceof Parcelle && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
                         return 6;
                 }
                 break;
             case 18:
                 i=0;
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if (Couleur.JAUNE ==p.getComposant().getCouleur() &&  p.getNombreDeBambous()==3){
+                    if (Couleur.ROSE ==p.getComposant().getCouleur() &&  p.getNombreDeBambous()==3){
                         i++;
                     }
                 }
@@ -43,7 +43,7 @@ public class ObjectifJardinier extends Objectif {
             case 19:
                 i=0;
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if (Couleur.ROSE ==p.getComposant().getCouleur() &&  p.getNombreDeBambous()==3){
+                    if (Couleur.JAUNE ==p.getComposant().getCouleur() &&  p.getNombreDeBambous()==3){
                         i++;
                     }
                 }
@@ -78,37 +78,37 @@ public class ObjectifJardinier extends Objectif {
                 break;
             case 24:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof AmenagementEngrais && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
+                    if(p instanceof AmenagementEngrais && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
                         return 4;
                 }
                 break;
             case 25:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof AmenagementEnclos && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
+                    if(p instanceof AmenagementEnclos && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
                         return 5;
                 }
                 break;
             case 26:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof AmenagementEngrais && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
+                    if(p instanceof AmenagementEngrais && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
                         return 5;
                 }
                 break;
             case 27:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof AmenagementBassin && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
+                    if(p instanceof AmenagementBassin && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
                         return 6;
                 }
                 break;
             case 28:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof AmenagementEnclos && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
+                    if(p instanceof AmenagementEnclos && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
                         return 6;
                 }
                 break;
             case 29:
                 for (ComposantParcelle p : plateauDeJeu.getParcelles()){
-                    if(p instanceof Parcelle && p.getComposant().getCouleur()==Couleur.JAUNE && p.getNombreDeBambous()==4)
+                    if(p instanceof Parcelle && p.getComposant().getCouleur()==Couleur.ROSE && p.getNombreDeBambous()==4)
                         return 7;
                 }
                 break;
@@ -132,13 +132,13 @@ public class ObjectifJardinier extends Objectif {
 
         switch (identifiant) {
             case 16:
-                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement bassain, Points: 5";
+                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement bassain, Points: 5";
             case 17:
-                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose sans amenagement, Points: 6";
+                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune sans amenagement, Points: 6";
             case 18:
-                return "Objectif " + identifiant + " : Plus de 2 parcelles jaunes contiennent exactement 3 bambous jaunes chacune, Points: 6";
+                return "Objectif " + identifiant + " : Plus de 2 parcelles roses contiennent exactement 3 bambous roses chacune, Points: 6";
             case 19:
-                return "Objectif " + identifiant + " : Plus de 3 parcelles roses contiennent exactement 3 bambous roses chacune, Points: 7";
+                return "Objectif " + identifiant + " : Plus de 3 parcelles jaunes contiennent exactement 3 bambous jaunes chacune, Points: 7";
             case 20:
                 return "Objectif " + identifiant + " : Plus de 4 parcelles vertes contiennent exactement 3 bambous verts chacune, Points: 8";
             case 21:
@@ -148,17 +148,17 @@ public class ObjectifJardinier extends Objectif {
             case 23:
                 return "Objectif " + identifiant + " : Quatre bambous verts sur une parcelle verte sans amenagement, Points: 5";
             case 24:
-                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement engrais, Points: 4";
+                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement engrais, Points: 4";
             case 25:
-                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement enclos, Points: 5";
+                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement enclos, Points: 5";
             case 26:
-                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement engrais, Points: 5";
+                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement engrais, Points: 5";
             case 27:
-                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement bassin, Points: 6";
+                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement bassin, Points: 6";
             case 28:
-                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune avec amenagement enclos, Points: 6";
+                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose avec amenagement enclos, Points: 6";
             case 29:
-                return "Objectif " + identifiant + " : Quatre bambous jaunes sur une parcelle jaune sans amenagement, Points: 7";
+                return "Objectif " + identifiant + " : Quatre bambous roses sur une parcelle rose sans amenagement, Points: 7";
             case 30:
                 return "Objectif " + identifiant + " : Quatre bambous verts sur une parcelle verte avec amenagement engrais, Points: 3";
         }
--- src/test/java/uqam/inf5153/game/objectif/ObjectifJardinierTest.java
+++ src/test/java/uqam/inf5153/game/objectif/ObjectifJardinierTest.java
@@ -1,12 +1,33 @@
 package uqam.inf5153.game.objectif;
 
 import org.junit.Test;
+import uqam.inf5153.game.Jeu;
+import uqam.inf5153.game.actions.PlaceParcelle;
+import uqam.inf5153.game.plateau.PlateauDeJeu;
+import uqam.inf5153.game.tuile.ComposantParcelle;
+import uqam.inf5153.game.tuile.parcelle.ParcelleJaune;
+import uqam.inf5153.game.tuile.parcelle.ParcelleVerte;
 
 import static org.junit.Assert.*;
 
 public class ObjectifJardinierTest {
 
-    @Test
-    public void appliquerObjectif() {
-    }
+        @Test
+        public void testAppliquerObjectif_1() {
+            //4 bambous jaunes sur une parcelle jaune avec amenagement bassain point=5
+            PlateauDeJeu plateau =new PlateauDeJeu();
+            ComposantParcelle p1=new ParcelleJaune();
+            Jeu jeu=new Jeu();
+            p1.getComposant().setIrriguee(true);
+            p1.fairePousserBambou();
+            p1.fairePousserBambou();
+            p1.fairePousserBambou();
+            p1.fairePousserBambou();
+            PlaceParcelle placeParcelle=new PlaceParcelle(jeu);
+            placeParcelle.placerParcelleDansPlateau(plateau,p1,2,1);
+            ObjectifParcelle objectif =new ObjectifParcelle(16);
+            assertEquals(objectif.appliquerObjectif(plateau), 5);
+
+        }
+
 }
